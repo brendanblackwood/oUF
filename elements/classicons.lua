95b20f922742c9aeee760b102b41b79f976a0d77
Adrian L Lange <mail@p3lim.net>
2016-05-31 17:41:30 +0200

    classicons: Add Combo Points for rogues and druids

--- elements/classicons.lua
+++ elements/classicons.lua
@@ -10,9 +10,11 @@
 
  Notes
 
+ Druid   - Combo Points
  Mage    - Arcane Charges
  Monk    - Chi Orbs
  Paladin - Holy Power
+ Rogue   - Combo Points
  Warlock - Soul Shards
 
  Examples
@@ -233,6 +235,9 @@ do
 			RequireSpec = SPEC_WARLOCK_AFFLICTION
 			RequireSpell = WARLOCK_SOULBURN
 		end
+	elseif(PlayerClass == 'ROGUE' or PlayerClass == 'DRUID') then
+		ClassPowerID = SPELL_POWER_COMBO_POINTS
+		ClassPowerType = 'COMBO_POINTS'
 	elseif(PlayerClass == 'MAGE' and isBetaClient) then
 		ClassPowerID = SPELL_POWER_ARCANE_CHARGES
 		ClassPowerType = 'ARCANE_CHARGES'
